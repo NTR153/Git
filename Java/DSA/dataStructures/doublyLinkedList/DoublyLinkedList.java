8492173674b6f93ef688e3c0db6bd8475645763e
ntr_ria <nirmal.rathod@riaadvisory.com>
2024-03-05 17:18:35 +0530

    doubly linked list

---
+++ Java/DSA/dataStructures/doublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,44 @@
+package dataStructures.doublyLinkedList;
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node (int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node tempNode = head;
+        while (tempNode != null) {
+            System.out.println(tempNode.value);
+            tempNode = tempNode.next;
+        }
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
---
+++ Java/DSA/dataStructures/doublyLinkedList/Main.java
@@ -0,0 +1,8 @@
+package dataStructures.doublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList tempList = new DoublyLinkedList(1);
+        tempList.printList();
+    }
+}
