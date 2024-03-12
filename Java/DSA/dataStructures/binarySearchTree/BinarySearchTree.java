b2edbbf2323108a2b75c67029f8ed3fb61113919
ntr_ria <nirmal.rathod@riaadvisory.com>
2024-03-12 21:40:56 +0530

    insert and contains

--- Java/DSA/dataStructures/binarySearchTree/BinarySearchTree.java
+++ Java/DSA/dataStructures/binarySearchTree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package dataStructures.binarySearchTree;
 
+import dataStructures.temp;
+
 public class BinarySearchTree {
     // Full tree -> Either zero or two nodes on every node
     // Perfect tree -> any level on the tree that has any node/s is completely filled across
@@ -13,6 +15,8 @@ public class BinarySearchTree {
     // remove() by value is O(n), O(log n)
     // respectively for linked list and BST
 
+    // BST can not have two nodes with same value
+
     Node root;
 
     class Node {
@@ -23,5 +27,42 @@ public class BinarySearchTree {
         Node (int value) {
             this.value = value;
         }
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root==null) {
+            root=newNode;
+            return true;
+        }
+        Node tempNode = root;
+
+        while (true) {
+            if (value<tempNode.value) {
+                if (tempNode.left!=null) tempNode=tempNode.left;
+                else {
+                    tempNode.left = newNode;
+                    return true;
+                } 
+            } else if (value>tempNode.value) {
+                if (tempNode.right!=null) tempNode=tempNode.right;
+                else  {
+                    tempNode.right = newNode;
+                    return true;
+                } 
+            } else if (value==tempNode.value) {
+                return false;
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        Node tempNode = root;
+        while (tempNode!=null) {
+            if (value<tempNode.value) tempNode=tempNode.left; 
+            else if (value>tempNode.value) tempNode=tempNode.right;
+            else if (value==tempNode.value) return true;
+        }
+        return false;
     }
 }
--- Java/DSA/dataStructures/binarySearchTree/Main.java
+++ Java/DSA/dataStructures/binarySearchTree/Main.java
@@ -3,6 +3,9 @@ package dataStructures.binarySearchTree;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree tempBST = new BinarySearchTree();
-        System.out.println("Root : " + tempBST.root);
+        tempBST.insert(2);
+        tempBST.insert(1);
+        tempBST.insert(3);
+        System.out.println(tempBST.contains(4));
     }
 }
