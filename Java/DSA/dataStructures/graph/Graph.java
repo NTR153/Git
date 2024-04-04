f83767efdee7870a9164684cf50395bbb51b0164
ntr_ria <nirmal.rathod@riaadvisory.com>
2024-04-04 12:58:57 +0530

    removeVertex

--- Java/DSA/dataStructures/graph/Graph.java
+++ Java/DSA/dataStructures/graph/Graph.java
@@ -27,6 +27,8 @@ public class Graph {
     //          e. Removing a vertex - O(|V|+|E|)
     // Adjacency List is more advantages from space complexity Perspective
 
+    // Following code is for bidirectional edges
+
     private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
 
     public void printGraph() {
@@ -36,6 +38,35 @@ public class Graph {
     public boolean addVertex(String vertex) {
         if (adjList.get(vertex) == null) {
             adjList.put(vertex, new ArrayList<String>());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addEdge(String vertex1, String vertex2) {
+        if (adjList.get(vertex1) != null && adjList.get(vertex2) != null) {
+            adjList.get(vertex1).add(vertex2);
+            adjList.get(vertex2).add(vertex1);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeEdge(String vertex1, String vertex2) {
+        if (adjList.get(vertex1) != null && adjList.get(vertex2) != null) {
+            adjList.get(vertex1).remove(vertex2);
+            adjList.get(vertex2).remove(vertex1);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeVertex(String vertex) {
+        if (adjList.get(vertex) != null) {
+            for (String otherVertex : adjList.get(vertex)) {
+                adjList.get(otherVertex).remove(vertex);
+            }
+            adjList.remove(vertex);
             return true;
         }
         return false;
