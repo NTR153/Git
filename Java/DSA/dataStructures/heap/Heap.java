144d33da0009cc4d07863a29c8452a54c618b312
ntr_ria <nirmal.rathod@riaadvisory.com>
2024-04-09 10:48:45 +0530

    remove and sinkdown

--- Java/DSA/dataStructures/heap/Heap.java
+++ Java/DSA/dataStructures/heap/Heap.java
@@ -46,6 +46,25 @@ public class Heap {
         heap.set(index2, temp);
     }
 
+    private void sinkDown(int index) {
+        int maxIndex = index;
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+
+            if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+                maxIndex = leftIndex;
+            }
+            if (rightIndex < heap.size() && heap.get(leftIndex) < heap.get(maxIndex)) {
+                maxIndex = rightIndex;
+            }
+            if (maxIndex != index) {
+                swap(index, maxIndex);
+                index = maxIndex;
+            } else return;
+        }
+    }
+
     public void insert(int value) {
         heap.add(value);
         int current = heap.size() - 1;
@@ -53,5 +72,14 @@ public class Heap {
             swap(current, parent(current));
             current = parent(current);
         }
+    }
+
+    public Integer remove() {
+        if (heap.size() == 0) return null;
+        if (heap.size() == 1) return heap.remove(0);
+        int maxValue = heap.get(0);
+        heap.set(0, heap.remove(heap.size() - 1));
+        sinkDown(0);
+        return maxValue;
     }
 }
--- Java/DSA/dataStructures/heap/Main.java
+++ Java/DSA/dataStructures/heap/Main.java
@@ -3,14 +3,17 @@ package dataStructures.heap;
 public class Main {
     public static void main(String[] args) {
         Heap myHeap = new Heap();
-        myHeap.insert(99);
-        myHeap.insert(72);
-        myHeap.insert(61);
-        myHeap.insert(58);
-        System.out.println(myHeap.getHeap());
-        myHeap.insert(100);
-        System.out.println(myHeap.getHeap());
+        myHeap.insert(95);
         myHeap.insert(75);
+        myHeap.insert(80);
+        myHeap.insert(55);
+        myHeap.insert(60);
+        myHeap.insert(50);
+        myHeap.insert(65);
+        System.out.println(myHeap.getHeap());
+        myHeap.remove();
+        System.out.println(myHeap.getHeap());
+        myHeap.remove();
         System.out.println(myHeap.getHeap());
     }
 }
