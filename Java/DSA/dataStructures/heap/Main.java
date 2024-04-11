27195b78701e63ffbf1fd070d03f6737cdac33b3
ntr_ria <nirmal.rathod@riaadvisory.com>
2024-04-11 16:48:14 +0530

    streamMax

--- Java/DSA/dataStructures/heap/Main.java
+++ Java/DSA/dataStructures/heap/Main.java
@@ -1,7 +1,12 @@
 package dataStructures.heap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        /* findKthSmallest
+        
         int[] nums1 = {7, 10, 4, 3, 20, 15};
         int k1 = 3;
         System.out.println("Test case 1:");
@@ -24,6 +29,33 @@ public class Main {
         System.out.println("Expected output: 7");
         System.out.println("Actual output: " + findKthSmallest(nums3, k3));
         System.out.println();
+        */
+
+        /* streamMax
+
+        int[] nums1 = {1, 5, 2, 9, 3, 6, 8};
+        System.out.println("Test case 1:");
+        System.out.println("Input: [1, 5, 2, 9, 3, 6, 8]");
+        System.out.println("Expected output: [1, 5, 5, 9, 9, 9, 9]");
+        System.out.println("Actual output: " + streamMax(nums1));
+        System.out.println();
+    
+        // Test case 2
+        int[] nums2 = {10, 2, 5, 1, 0, 11, 6};
+        System.out.println("Test case 2:");
+        System.out.println("Input: [10, 2, 5, 1, 0, 11, 6]");
+        System.out.println("Expected output: [10, 10, 10, 10, 10, 11, 11]");
+        System.out.println("Actual output: " + streamMax(nums2));
+        System.out.println();
+    
+        // Test case 3
+        int[] nums3 = {3, 3, 3, 3, 3};
+        System.out.println("Test case 3:");
+        System.out.println("Input: [3, 3, 3, 3, 3]");
+        System.out.println("Expected output: [3, 3, 3, 3, 3]");
+        System.out.println("Actual output: " + streamMax(nums3));
+        System.out.println();
+         */
     }
 
     public static int findKthSmallest(int[] nums, int k) {
@@ -47,4 +79,20 @@ public class Main {
     //     }
     //     return maxHeap.remove();
     // }
+
+    // Implement a function named streamMax that processes a stream of integers 
+    // and returns the maximum number encountered so far for each input number.
+
+    public static List<Integer> streamMax(int[] nums) {
+        Heap maxHeap = new Heap();
+        List<Integer> maxStream = new ArrayList<>();
+ 
+        for (int num : nums) {
+            maxHeap.insert(num);
+            // The heap's root is always the maximum, so we add it to the result list
+            maxStream.add(maxHeap.getHeap().get(0));
+        }
+ 
+        return maxStream;
+    }
 }
